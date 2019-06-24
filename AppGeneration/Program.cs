61602b58fefc6b6c2a86755c3e0f7a01df51560d
Polina Tarasova <p.tarasova@yadro.com>
2019-06-24 18:21:59 +0300

    Paths fixed

--- AppGeneration/Program.cs
+++ AppGeneration/Program.cs
@@ -13,10 +13,10 @@ namespace AppGeneration
         private static string modelFileName = "generatedModel.xml";
         private static string classForGeneration = "LectureListActivity.java";
         private static string classTemplate = "LectureListActivityTemplate.cshtml";
-        private static string pathToDirectoryWithFeatures = "/Users/p.tarasova/RiderProjects/AppGeneration/AppGeneration/adminapp";
+        private static string pathToDirectoryWithFeatures = "adminapp";
 
         private static string pathToTargetDirectory =
-            "/Users/p.tarasova/RiderProjects/AppGeneration/AppGeneration/AppForGeneration/AdminApp/app/src/main/java/com";
+            "AppForGeneration/AdminApp/app/src/main/java/com";
 
 
         static void Main(string[] args)
